cd0f13b83d568aac77b1adf0d3d6dbef944a282a
lsh9401 <l940131@gmail.com>
2020-09-09 13:43:36 +0900

    Clean up

--- src/Common/Utils/Http.js
+++ src/Common/Utils/Http.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const prefix = localStorage.getItem('keyPrefix');
 const user = localStorage.getItem('userName');
 const idToken = localStorage.getItem(`${prefix}.${user}.idToken`);
+const apiId = 'your_api_id';
 
 const header = {
 	'Authorization': `Bearer ${idToken}`,
 	'Content-Type': `application/json`,
 };
-
 const Http = axios.create({
-	baseURL: 'https://anc2s2p626.execute-api.ap-northeast-2.amazonaws.com/latest',
+	baseURL: `https://${apiId}.execute-api.ap-northeast-2.amazonaws.com/latest`,
 	headers: header,
 });
 
